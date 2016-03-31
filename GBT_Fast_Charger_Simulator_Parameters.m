1666d0f6cf68d1c74e10fe868a14f08e83c1592d
willfansh <willfansh@hotmail.com>
2016-03-31 14:53:41 +0800

    initial upload project to github

---
+++ GBT_Fast_Charger_Simulator_Parameters.m
@@ -0,0 +1,10 @@
+disp('Load CSC Simulator Parameters......');
+
+SampleTimeSec = 0.01;
+STD_ON = true;
+STD_OFF = false;
+STD_ACTIVE = true;
+STD_IDLE = false;
+
+disp('Load CSC Simulator Parameters...... Done!');
+
